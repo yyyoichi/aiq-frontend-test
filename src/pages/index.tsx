6ce72744a3e260b386a1a2bc1fbe5a622fb85189
yyyoichi <origamiichiro@gmail.com>
2022-11-28 12:39:25 +0900

    add: 都道府県リストから動的にチェックボックスを生成する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import fetcher from 'src/lib/fetcher';
 import styles from '../styles/Home.module.css';
-
+type Pref = {
+  prefCode: number;
+  prefName: string;
+};
+type JSONPref = {
+  message: string | null;
+  result: Pref[];
+};
+type PopulationCompo = { name: string; value: number[] };
+type JSONPopCmp = {
+  message: string | null;
+  result: {
+    boundaryYear: number;
+    data: { label: string; data: { year: number; value: number }[] }[];
+  };
+};
 export default function Home() {
+  const [pref, setPref] = useState<Pref[]>([]);
+  useEffect(() => {
+    // 1．RESAS(地域経済分析システム) APIの「都道府県一覧」APIから取得する
+    const url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
+    fetcher<JSONPref>(url)
+      .then((res) => setPref(res.result))
+      .catch((e) => console.error(e));
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +38,19 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <h1>都道府県別の総人口推移グラフを表示するSPA(Single Page Application)を構築せよ</h1>
+      <div>
+        {
+          // 2．APIレスポンスから都道府県一覧のチェックボックスを動的に生成する
+          pref.map((x, i) => {
+            return (
+              <label key={i}>
+                <input type='checkbox' value={x.prefCode} />
+                {x.prefName}
+              </label>
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
